791dcdf041bc5df612541b95cc3be5e23e02ec7f
khrisnaa <igdkhrisnaaa@gmail.com>
2025-11-15 12:02:27 +0800

    feat: update index.ts too serveless app

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,27 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth';
-import expenseRoutes from './routes/expense';
+import authRoutes from './routes/auth.js';
+import expenseRoutes from './routes/expense.js';
 
 dotenv.config();
 
 const app: Application = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-// Routes
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/auth', authRoutes);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Walleto API is running  🚀');
+  res.send('Walleto API is running 🚀');
 });
 
-// MongoDB connection
 mongoose
   .connect(process.env.APP_MONGO_URI!)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-const PORT = process.env.APP_PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
